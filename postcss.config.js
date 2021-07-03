cb9f3e09efa878259d399b0adaf33d3c55cf760f
wubing <wubing@123456>
2021-07-03 14:13:41 +0800

    添加文件
    Signed-off-by: wubing <wubing@123456>

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: {
+    // 兼容浏览器，添加前缀
+    autoprefixer: {
+      overrideBrowserslist: [
+        'Android 4.1',
+        'iOS 7.1',
+        'Chrome > 31',
+        'ff > 31',
+        'ie >= 8',
+        'last 10 versions' // 所有主流浏览器最近10版本用
+      ],
+      grid: true
+    },
+    'postcss-pxtorem': {
+      rootValue: 75, // 设计稿宽度的1/10
+      unitPrecision: 6, // 小数位
+      minPixelValue: 1, // 转换的最小单位
+      selectorBlackList: ['.mint-', '.mi-'], // 忽略的样式, 正则
+      mediaQuery: false, // (Boolean) 媒体查询里的单位是否需要转换单位
+      exclude: [/^node_modules$/], // (Array or Regexp) 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+      propList: ['*'] // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部，正则
+    }
+  }
+}
---
+++ vue.config.js
@@ -0,0 +1,96 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  // 部署应用包时的基本 URL
+  publicPath: '/',
+  // 运行 vue-cli-service build 时生成的生产环境构建文件的目录
+  // 默认构建前清除文件夹(构建时传入 --no-clean 可关闭该行为
+  outputDir: 'dist',
+
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: 'static',
+
+  // 指定生成的 index.html 的输出路径 (相对于 outputDir),也可以是一个绝对路径
+  indexPath: 'index.html',
+
+  // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
+  filenameHashing: true,
+
+  // 生产环境 sourceMap
+  productionSourceMap: false,
+
+  devServer: {
+    open: false,
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: {
+      '^/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true, // 是否跨域
+        ws: true, // 是否代理 webSockets
+        // secure: true, //是否https接口
+        pathRewrite: {
+          // 路径重置
+          '^/api': '/api'
+        }
+      }
+    }
+  },
+
+  // outputDir: 'dist', // 打包目录
+  configureWebpack: config => {
+    // webpack自定义配置
+    // 生产环境打包分析体积
+    if (process.env.NODE_ENV === 'prod') {
+      // 为生产环境修改配置...
+      config.mode = 'production'
+      config.optimization = {
+        runtimeChunk: 'single', // 将webpack运行时生成代码打包到runtime.js
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000, // 依赖包超过20000bit将被单独打包
+          cacheGroups: {
+            vendors: {
+              name: 'vendors',
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              chunks: 'initial'
+            },
+            common: {
+              name: 'common',
+              minChunks: 2,
+              priority: -20,
+              chunks: 'initial',
+              reuseExistingChunk: true
+            }
+          }
+        }
+      }
+    }
+  },
+  chainWebpack: config => {
+    // CLI内部webpack配置，添加全局less文件
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type =>
+      addStyleResource(config.module.rule('less').oneOf(type))
+    )
+  }
+}
+
+// 全局样式 变量、函数
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        resolve('src/styles/variables.less'),
+        resolve('src/styles/mixin.less')
+      ]
+    })
+}
